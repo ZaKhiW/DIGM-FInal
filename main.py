33f8937d61e687e95c10a83a6123c08579e6b136
ZaKhiW <zkw32@drexel.edu>
2026-05-14 14:44:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-"""main.py -  Floor Plan Generator
+""
+"main.py -  Floor Plan Generator
 
 File containing the entire code string for the scene. This file acts as thr main file 
 containing the entirety of the code meant for the Floor Plan Generator.
