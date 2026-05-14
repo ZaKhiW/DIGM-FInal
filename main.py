b772ca0ed944525ff3191c7aaef35c982e61bbbe
ZaKhiW <zkw32@drexel.edu>
2026-05-14 14:46:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-""
-"main.py -  Floor Plan Generator
+"""
+main.py -  Floor Plan Generator
 
 File containing the entire code string for the scene. This file acts as thr main file 
 containing the entirety of the code meant for the Floor Plan Generator.
 
 Assembles complete Floor Plan Generator.
 """
+
+import os
+import sys
+import maya.cmds as cmds
