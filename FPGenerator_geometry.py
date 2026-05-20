837c0bf6709b999651c9fb11f5f13786ac4072ed
ZaKhiW <zkw32@drexel.edu>
2026-05-20 15:55:48 -0400

    Update FPGenerator_geometry.py

--- FPGenerator_geometry.py
+++ FPGenerator_geometry.py
@@ -7,6 +7,7 @@ name(s). No materials or scene logic included.
 Usage:
   Import FPGenerator_geometry as geo
   geo.create_wall()
+  geo.create_floor()
 
 """
 
@@ -55,36 +56,3 @@ def create_floor(self):
         )[0]
 
         cmds.move(0, 0, 0, floor)
-
- def build_room(self, x, z, w, d):
-        t = self.wall_thickness
-
-        # Top
-        self.create_wall(x, z + d/2, w, t)
-
-        # Bottom
-        self.create_wall(x, z - d/2, w, t)
-
-        # Left
-        self.create_wall(x - w/2, z, t, d)
-
-        # Right
-        self.create_wall(x + w/2, z, t, d)
-
-    def generate_hallways(self):
-        for i in range(len(self.rooms)-1):
-            if random.random() < self.hallway_chance:
-                x1, z1, _, _ = self.rooms[i]
-                x2, z2, _, _ = self.rooms[i+1]
-
-                midx = (x1 + x2) / 2
-                midz = (z1 + z2) / 2
-
-                self.create_wall(
-                    midx,
-                    midz,
-                    abs(x1-x2)+2,
-                    self.wall_thickness
-                )
-
-
